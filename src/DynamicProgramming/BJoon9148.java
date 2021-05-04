9c550fc356199257b336ded18f4cbc3d6ccf1afe
ssung <qkrtjdcjf124@gmail.com>
2021-05-04 19:42:51 +0900

    백준 DP문제들 업로드

---
+++ src/DynamicProgramming/BJoon9148.java
@@ -0,0 +1,17 @@
+package DynamicProgramming;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 신나는 함수 실행
+ * Created by qkrtjdcjf124
+ * Date : 2021/05/04
+ * 
+ */
+public class BJoon9148 {
+
+	public static void main(String[] args) {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+	}
+
+}
