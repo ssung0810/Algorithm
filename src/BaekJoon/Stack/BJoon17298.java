a7c5b200e45414256a3aac3ec94fee4236998593
ssung <qkrtjdcjf124@gmail.com>
2021-06-14 13:56:25 +0900

    BaekJoon Update(Stack)

--- src/BaekJoon/Stack/BJoon17298.java
+++ src/BaekJoon/Stack/BJoon17298.java
@@ -3,12 +3,12 @@ package BaekJoon.Stack;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 import java.util.Stack;
+import java.util.StringTokenizer;
 
 /* 오큰수
  * Created by qkrtjdcjf124
- * Date : 2021/06/11
+ * Date : 2021/06/14
  */
 public class BJoon17298 {
 
@@ -19,13 +19,33 @@ public class BJoon17298 {
 		Stack<Integer> s = new Stack<>();
 		
 		int N = Integer.parseInt(bf.readLine());
+		int[] arr = new int[N];
 		
 		st = new StringTokenizer(bf.readLine());
 		for(int i=0; i<N; i++) {
-			s.push(Integer.parseInt(st.nextToken()));
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		
+		s.push(0);
+		for(int i=1; i<N; i++) {
+			if(arr[s.peek()] >= arr[i]) {
+				s.push(i);
+			} else {
+				while(!s.empty() && arr[s.peek()] < arr[i]) {
+					arr[s.pop()] = arr[i];
+				}
+				
+				s.push(i);
+			}
+		}
 		
+		while(!s.empty()) {
+			arr[s.pop()] = -1;
+		}
+		
+		for(int i=0; i<N; i++) {
+			sb.append(arr[i] + " ");
+		}
 		
 		System.out.println(sb);
 	}
