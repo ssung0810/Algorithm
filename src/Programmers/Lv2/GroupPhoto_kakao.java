49c8c636daff302e606806c11a094e4c1e826f3e
ssung <qkrtjdcjf124@gmail.com>
2021-12-22 15:21:36 +0900

    Programmers Update(Lv2)

--- src/Programmers/Lv2/GroupPhoto_kakao.java
+++ src/Programmers/Lv2/GroupPhoto_kakao.java
@@ -6,9 +6,11 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 /* 단체사진 찍기
- * Date : 2021/08/06
+ * Date : 2021/12/22
  */
 public class GroupPhoto_kakao {
+	static private boolean[] visit = new boolean[8];
+	static private int answer = 0;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
@@ -16,15 +18,51 @@ public class GroupPhoto_kakao {
 		int n = Integer.parseInt(bf.readLine());
 		String[] data = new String[n];
 		
-		StringTokenizer st = new StringTokenizer(bf.readLine());
 		for(int i=0; i<n; i++) {
-			data[i] = st.nextToken();
+			data[i] = bf.readLine();
 		}
 		
 		////////////////////////////////////
 
+		String[] member = {"A", "C", "F", "J", "M", "N", "R", "T"};
+		photo(data, member, new StringBuilder());
 
+		System.out.println(answer);
 	}
 
+	static private void photo(String[] data, String[] member, StringBuilder sb) {
+		if(sb.length() > 7) {
+			if (group(data, sb.toString())) answer++;
+			return;
+		}
 
+		for(int i=0; i<8; i++) {
+			if(!visit[i]) {
+				sb.append(member[i]);
+				visit[i] = true;
+				photo(data, member, sb);
+				visit[i] = false;
+				sb.deleteCharAt(sb.length() - 1);
+			}
+		}
+	}
+
+	static private boolean group(String[] data, String member) {
+		for(String t : data) {
+			int member1 = member.indexOf(t.charAt(0));
+			int member2 = member.indexOf(t.charAt(2));
+			char hope = t.charAt(3);
+			int distance = t.charAt(4) - '0';
+
+			if(hope == '<') {
+				if(Math.abs(member2 - member1)-1 >= distance) return false;
+			} else if(hope == '>') {
+				if(Math.abs(member2 - member1)-1 <= distance) return false;
+			} else {
+				if(Math.abs(member2 - member1)-1 != distance) return false;
+			}
+		}
+
+		return true;
+	}
 }
--- src/Programmers/Main2.java
+++ src/Programmers/Main2.java
@@ -7,59 +7,11 @@ import java.util.*;
 
 public class Main2 {
 	public static void main(String[] args) throws IOException {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 
-		String s = bf.readLine();
+		sb.append("1");
+		sb.append("2");
 
-		int min = s.length();
-
-//		for (int i = 1; i <= s.length() / 2; i++) {
-			String t = compression(s, 1);
-			int compLeng = t.length();
-			min = Math.min(min, compLeng);
-
-			System.out.println(t);
-//		}
-
-		System.out.println(min);
-	}
-
-	static String compression(String str, int i) {
-
-		int count = 1;
-		String compression = "";
-		String pattern = "";
-
-		for (int j = 0; j <= str.length() + i; j += i) {
-
-			String nowStr;
-
-			// 전 문자열과 비교할 현재 문자열
-			if (j >= str.length()) { // 현재 문자열이 없을 때
-				nowStr = "";
-			} else if (str.length() < j + i) { // 마지막 현재 문자열일 때
-				nowStr = str.substring(j);
-			} else {
-				nowStr = str.substring(j, j + i); // 그 외
-			}
-
-			System.out.println(nowStr);
-
-			// 1. 전 문자열이랑 똑같은지 비교한다. (맨 처음이면 비교 X)
-			if (j != 0) {
-				if (nowStr.equals(pattern)) { // 똑같으면
-					count++;
-				} else if (count >= 2) { // 다르고 count가 2회 이상이면 압축 가능
-					compression += count + pattern;
-					count = 1;
-				} else { // 압축 불가능하면 그냥 그대로 문자열 이어붙이기
-					compression += pattern;
-				}
-			}
-			// 2. i 길이만큼 문자열을 자른다.
-			pattern = nowStr;
-		}
-
-		return compression;
+		System.out.println(sb.length());
 	}
 }
